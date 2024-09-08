74e4bfbaf2ce0624b66f6706c4623b7e961c8487
harsh9o9 <hbhardwaj454@gmail.com>
2024-09-08 21:01:08 +0530

    Updated upload video flow to show progress of video upload in %age

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import fs from "fs-extra";
-import ora from "ora";
 import path from "path";
 import { getAuthToken } from "./auth.js";
 import { addToPlaylist, createPlaylist, getAllPlaylists } from "./playlist.js";
@@ -10,49 +9,73 @@ import {
   promptForNewPlaylistName,
   promptForPlaylistChoice,
 } from "./utils/inquirerPrompts.js";
-import { logInfo } from "./utils/logger.js";
+import { logError, logInfo, logSuccess } from "./utils/logger.js";
 
 async function uploadVideosFromFolder(folderPath) {
-  const files = fs
-    .readdirSync(folderPath)
-    .filter((file) =>
-      [".mp4", ".mov", ".avi"].includes(path.extname(file).toLowerCase())
-    );
+  try {
+    // Ensure the folder exists and contains video files
+    const files = fs
+      .readdirSync(folderPath)
+      .filter((file) =>
+        [".mp4", ".mov", ".avi"].includes(path.extname(file).toLowerCase())
+      );
 
-  const auth = await getAuthToken();
-  const choice = await promptForPlaylistChoice();
-
-  let playlistId;
-  if (choice === "new") {
-    const playlistName = await promptForNewPlaylistName();
-    playlistId = await createPlaylist(auth, playlistName);
-  } else if (choice === "existing") {
-    // Get all existing playlists
-    const playlists = await getAllPlaylists(auth);
-
-    if (playlists.length === 0) {
-      logInfo("No existing playlists found. You need to create one.");
-      const playlistName = await promptForNewPlaylistName();
-      playlistId = await createPlaylist(oauth2Client, playlistName);
-    } else {
-      // Show existing playlists and let the user choose
-      playlistId = await promptForExistingPlaylist(playlists);
+    if (files.length === 0) {
+      throw new Error("No video files found in the specified folder.");
     }
-  } else {
-    throw new Error("Invalid choice");
-  }
 
-  const spinner = ora("Uploading videos...").start();
-  for (const file of files) {
-    const filePath = path.join(folderPath, file);
-    const videoId = await uploadVideo(auth, filePath);
-    await addToPlaylist(auth, videoId, playlistId);
+    const auth = await getAuthToken();
+    const choice = await promptForPlaylistChoice();
+
+    let playlistId;
+    if (choice === "new") {
+      const playlistName = await promptForNewPlaylistName();
+      playlistId = await createPlaylist(auth, playlistName);
+    } else if (choice === "existing") {
+      // Get all existing playlists
+      const playlists = await getAllPlaylists(auth);
+
+      if (playlists.length === 0) {
+        logInfo("No existing playlists found. You need to create one.");
+        const playlistName = await promptForNewPlaylistName();
+        playlistId = await createPlaylist(auth, playlistName);
+      } else {
+        // Show existing playlists and let the user choose
+        playlistId = await promptForExistingPlaylist(playlists);
+      }
+    } else {
+      throw new Error("Invalid choice");
+    }
+
+    let filesCount = files.length;
+    logInfo(`Found ${filesCount} video files to upload.`);
+    for (const file of files) {
+      const filePath = path.join(folderPath, file);
+
+      try {
+        const videoId = await uploadVideo(auth, filePath);
+        await addToPlaylist(auth, videoId, playlistId);
+      } catch (err) {
+        filesCount--;
+        logError(
+          `❌ Failed to upload or add video: ${file}. Error: ${err.message}`
+        );
+      }
+    }
+    logSuccess(
+      `✅ Upload complete!, ${filesCount}/${files.length} files uploaded.`
+    );
+  } catch (err) {
+    logError(`Error during video upload process: ${err.message}`);
   }
-  spinner.succeed("Upload complete!");
 }
 
 // Prompt for folder path and start the process
 (async () => {
-  const folderPath = await promptForFolderPath();
-  uploadVideosFromFolder(folderPath).catch(console.error);
+  try {
+    const folderPath = await promptForFolderPath();
+    await uploadVideosFromFolder(folderPath);
+  } catch (err) {
+    logError(`Error in main process: ${err.message}`);
+  }
 })();
--- src/upload.js
+++ src/upload.js
@@ -1,29 +1,49 @@
+import chalk from "chalk";
 import fs from "fs";
 import { google } from "googleapis";
+import ora from "ora";
 import path from "path";
 import { logError, logSuccess } from "./utils/logger.js";
 
 export async function uploadVideo(auth, filePath) {
   const youtube = google.youtube({ version: "v3", auth });
   const fileName = path.basename(filePath);
+  const fileSize = fs.statSync(filePath).size; // Get the file size for progress tracking
+  // Initialize the ora spinner for upload progress
+  const spinner = ora({
+    text: `Uploading: ${chalk.green(fileName)} - 0%`,
+    color: "blue",
+  }).start();
 
   try {
-    const res = await youtube.videos.insert({
-      part: "snippet,status",
-      requestBody: {
-        snippet: {
-          title: fileName,
-          description: "Uploaded via CLI",
-          categoryId: "22",
+    const res = await youtube.videos.insert(
+      {
+        part: "snippet,status",
+        requestBody: {
+          snippet: {
+            title: fileName,
+            description: "Uploaded via CLI",
+            categoryId: "22",
+          },
+          status: { privacyStatus: "unlisted" },
         },
-        status: { privacyStatus: "private" },
+        media: { body: fs.createReadStream(filePath) },
       },
-      media: { body: fs.createReadStream(filePath) },
-    });
-
+      {
+        // Track the progress of the video upload
+        onUploadProgress: (evt) => {
+          const progress = (evt.bytesRead / fileSize) * 100;
+          spinner.text = `Uploading: ${chalk.green(fileName)} - ${chalk.yellow(
+            progress.toFixed(2)
+          )}%`;
+        },
+      }
+    );
+    spinner.succeed(`Upload complete for: ${chalk.green(fileName)}`);
     logSuccess(`Uploaded video with id: ${res.data.id}`);
     return res.data.id;
   } catch (error) {
+    spinner.fail("Upload failed.");
     if (
       error.response &&
       error.response.status === 403 &&
